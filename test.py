858416673a56647be62d3f4fc8de5f0d053bc35a
Chris Choy <chrischoy@ai.stanford.edu>
2020-02-22 15:51:06 -0800

    backward test

--- test.py
+++ test.py
@@ -2,16 +2,25 @@ import torch
 from gesvd import GESVDFunction
 import numpy as np
 
-R = np.array([[0.41727819, -0.87345426, 0.25091147],
+R = np.array([[[0.41727819, -0.87345426, 0.25091147],
               [0.32246181, 0.40043949, 0.85771009],
-              [-0.84964539, -0.27699435, 0.44875031]])
+              [-0.84964539, -0.27699435, 0.44875031]],
+             [[0.41727819, -0.87345426, 0.25091147],
+              [0.32246181, 0.40043949, 0.85771009],
+              [-0.84964539, -0.27699435, 0.44875031]]])
 thR = torch.from_numpy(R)
-print(thR)
-U, S, V = np.linalg.svd(R)
-svd = GESVDFunction()
-thU, thS, thV = svd.apply(thR)
+thR.requires_grad_()
 
-print(np.allclose(U, thU))
-print(np.allclose(V, thV.t()))
-print(np.allclose(S, thS))
-print(torch.allclose(torch.matmul(thU, thV.t()), thR))
+print(thR)
+
+U, S, V = np.linalg.svd(R[0])
+svd = GESVDFunction()
+# Batch SVD
+thU, thS, thV = svd.apply(thR)
+(thS).sum().backward()
+print(thR.grad)
+
+print(np.allclose(U, thU[0].detach()))
+print(np.allclose(V, thV[0].detach().t()))
+print(np.allclose(S, thS[0].detach()))
+print(torch.allclose(torch.matmul(thU[0], thV[0].t()), thR[0]))
