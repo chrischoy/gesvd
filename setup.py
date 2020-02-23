3ce0e8e7c9d302b663977b69b7c044fbd4a709a0
Chris Choy <chrischoy@ai.stanford.edu>
2020-02-22 16:09:49 -0800

    fix minor setup config

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 setup(
-    name='gesvd_cpp',
+    name='gesvd',
+    version="0.1",
     ext_modules=[
         CppExtension('gesvd_cpp', ['gesvd.cpp']),
     ],
+    scripts=['gesvd.py', '__init__.py'],
     cmdclass={
         'build_ext': BuildExtension
     })
